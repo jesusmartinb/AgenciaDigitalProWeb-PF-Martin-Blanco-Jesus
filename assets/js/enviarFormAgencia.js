86d7e79e76cda1c0ea45f0bf80e8cc9f91c32e88
Jesus Martin Blanco <jesusmartinb64@gmail.com>
2022-09-08 11:46:24 +0200

    Se implementa EmailJS para el formulario de la pagina agencia.html y se crea el archivo enviarFormAgencia.js dentro de la carpeta assets/js

---
+++ assets/js/enviarFormAgencia.js
@@ -0,0 +1,41 @@
+const btn = document.getElementById('button');
+const nombre = document.getElementById('nombre');
+const telefono = document.getElementById('tel');
+const email = document.getElementById('email');
+const vacante = document.getElementById('vacante');
+const actual = document.getElementById('actual');
+const portfolio = document.getElementById('portfolio');
+const cv = document.getElementById('cv');
+
+document.getElementById('form').addEventListener('submit', function (event) {
+	event.preventDefault();
+
+	btn.value = 'Enviando...';
+
+	const serviceID = 'service_l8cyowp';
+	const templateID = 'template_jzeozyh';
+
+	emailjs.sendForm(serviceID, templateID, this)
+		.then(() => {
+			btn.value = 'Enviar Candidatura';
+			nombre.value = '';
+			telefono.value = '';
+			email.value = '';
+			vacante.value = '';
+			actual.value = '';
+			portfolio.value = '';
+			cv.value = '';
+
+			// Swal.fire(
+			// 	'Consulta enviada',
+			// 	'A la brevedad nos comunicaremos',
+			// 	'success'
+			// )
+
+		}, (err) => {
+			btn.value = 'Enviar';
+			alert(JSON.stringify(err));
+		});
+});
+
+emailjs.init('WUVodRdv02zVPiGM2');
